8a57a19be8c649b2fa9794627be186a951127c46
Matthias Wauer <matthiaswauer@gmail.com>
2022-02-14 19:18:45 +0100

    mgmt: os: add function docs

--- src/mcumgr/mgmt_os.h
+++ src/mcumgr/mgmt_os.h
@@ -11,18 +11,37 @@
 
 /* core OS commands */
 
-/* OS ECHO */
+/**
+ * @brief echo request parameters
+ *
+ */
 struct mgmt_echo_req {
     const char *echo_str;
 };
 
-#define ECHO_STR_MAX_LEN 128
+#define ECHO_STR_MAX_LEN 128 /**< Maximum allowed echo request string length */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief echo response values
+ *
+ */
 struct mgmt_echo_rsp {
-    int64_t mgmt_rc;
-    char echo_str[ECHO_STR_MAX_LEN + 1];
+    int64_t mgmt_rc; /**< Mgmt response code */
+    char echo_str[ECHO_STR_MAX_LEN + 1]; /**< Echo response string */
 };
 
+/**
+ * @brief Create encoded OS echo request packet
+ *
+ * @param buf       buffer where to store the request packet data
+ * @param sz        @p buf buffer size
+ * @param req       echo request parameters
+ * @return ssize_t  The size of the request stored in @p buf or -ENOBUFS, if the supplied buffer is to short for the encoded
+ */
 ssize_t mgmt_create_os_echo_req(uint8_t *buf, size_t sz, const struct mgmt_echo_req *req);
 
 /**
@@ -40,8 +59,33 @@ ssize_t mgmt_create_os_echo_req(uint8_t *buf, size_t sz, const struct mgmt_echo_
  */
 int mgmt_os_echo_decode_rsp(const uint8_t *buf, size_t sz, struct mgmt_echo_rsp *rsp);
 
-/* OS reset */
+/**
+ * @brief Create encoded OS reset request packet
+ *
+ * @param buf       buffer where to store the request packet data
+ * @param sz        @p buf buffer size
+ * @param req       echo request parameters
+ * @return ssize_t  The size of the request stored in @p buf or -ENOBUFS, if the supplied buffer is to short for the encoded
+ */
 ssize_t mgmt_create_os_reset_req(uint8_t *buf, size_t sz);
+
+/**
+ * @brief Check and decode the mgmt OS reset response SMP message
+ *
+ * @param buf       The buffer holding the message
+ * @param sz        Size of the buffer
+ * @param rsp       pointer where to save the decoded response.
+ * @return          0 on success and error code otherwise
+ *
+ * @retval 0 Successful execution, @p rsp is valid.
+ * @retval -EINVAL Argument validation failed
+ * @retval -ENODATA @p buf too short to hold SMP header or not a complete SMP packet.
+ * @retval -ENOMSG SMP payload decoding error or unexpected format, e.g. not a map, requested value has wrong format, ...
+ */
 int mgmt_os_reset_decode_rsp(uint8_t *buf, size_t sz, struct mgmt_rc *rsp);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
