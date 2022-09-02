39c601ee9f5a8c4828f839f2af5de9a3e8a42727
Matthias Wauer <matthiaswauer@gmail.com>
2022-09-02 20:49:47 +0200

    lib: hexlify: fix odd hexx string not rejected, add braces
    Fix failure to reject a odd number of ASCII characters hex string as invalid.
    Add braces around compount conditions.

--- src/mcumgr-client/lib/hexlify.c
+++ src/mcumgr-client/lib/hexlify.c
@@ -20,7 +20,7 @@ int hexlify(const uint8_t *data_buf, size_t data_sz, char *str_buf, size_t str_s
         return -EINVAL;
     }
 
-    for (i = 0; i < data_sz && str_sz > 2; ++i, str_sz -= 2, ++data_buf, str_buf += 2) {
+    for (i = 0; (i < data_sz) && (str_sz > 2); ++i, str_sz -= 2, ++data_buf, str_buf += 2) {
         sprintf(str_buf, "%02x", *data_buf);
     }
     if (i < data_sz) {
@@ -37,16 +37,19 @@ int unhexlify(const char *str_buf, uint8_t *data_buf, size_t data_sz)
         return -EINVAL;
     }
 
-    for (i = 0; i < data_sz && *str_buf != '\0'; ++i, str_buf += 2) {
+    for (i = 0; (i < data_sz) && (*str_buf != '\0'); ++i, str_buf += 2) {
         char sub[2];
-        for (int j = 0; j < 2 && str_buf[j] != '\0'; ++j) {
-            if (str_buf[j] >= '0' && str_buf[j] <= '9') {
+        if ((str_buf[0] != '\0') && (str_buf[1] == '\0')) {
+            return -EINVAL;
+        }
+        for (int j = 0; (j < 2) && (str_buf[j] != '\0'); ++j) {
+            if ((str_buf[j] >= '0') && (str_buf[j] <= '9')) {
                 sub[j] = '0';
-            } else if (str_buf[j] >= 'a' && str_buf[j] <= 'f') {
+            } else if ((str_buf[j] >= 'a') && (str_buf[j] <= 'f')) {
                 sub[j] = 'a' - 10;
-            } else if (str_buf[j] >= 'A' && str_buf[j] <= 'F') {
+            } else if ((str_buf[j] >= 'A') && (str_buf[j] <= 'F')) {
                 sub[j] = 'A' - 10;
-            } else if (str_buf[j] == '\0' && j == 0) {
+            } else if ((str_buf[j] == '\0') && (j == 0)) {
                 return i;
             } else {
                 return -EINVAL;
