67a0e7edb5445a85dd85de4e6e0a5d0d1e055851
Matthias Wauer <matthiaswauer@gmail.com>
2022-11-05 20:28:24 +0100

    mgmt: fix file/code formating

--- src/mcumgr-client/mgmt/mgmt_utils.c
+++ src/mcumgr-client/mgmt/mgmt_utils.c
@@ -21,117 +21,117 @@
  */
 int image_version_to_str(char *vbuf, const struct image_version *version)
 {
-    int ret = snprintf(vbuf, IMAGE_VERSION_STR_MAX, "%d.%d.%d",
-                       version->major, version->minor, version->revision);
+	int ret = snprintf(vbuf, IMAGE_VERSION_STR_MAX, "%d.%d.%d",
+			   version->major, version->minor, version->revision);
 
 
-    if (ret > 0 && ret < (int) IMAGE_VERSION_STR_MAX && version->build_num) {
-        snprintf(vbuf + ret , IMAGE_VERSION_STR_MAX - ret, "+%u", version->build_num);
-    }
-    vbuf[IMAGE_VERSION_STR_MAX - 1] = '\0';
+	if (ret > 0 && ret < (int) IMAGE_VERSION_STR_MAX && version->build_num) {
+		snprintf(vbuf + ret , IMAGE_VERSION_STR_MAX - ret, "+%u", version->build_num);
+	}
+	vbuf[IMAGE_VERSION_STR_MAX - 1] = '\0';
 
-    return 0;
+	return 0;
 }
 
 int image_hash_to_str(char *hbuf, const uint8_t hash[IMAGE_HASH_SIZE])
 {
-    hexlify(hash, IMAGE_HASH_SIZE, hbuf, IMAGE_HASH_STR_MAX);
-    return 0;
+	hexlify(hash, IMAGE_HASH_SIZE, hbuf, IMAGE_HASH_STR_MAX);
+	return 0;
 }
 
 int image_hash_str_to_buf(uint8_t hash[IMAGE_HASH_SIZE], const char *hash_str)
 {
-    int ret;
+	int ret;
 
-    if (strlen(hash_str) != (IMAGE_HASH_SIZE * 2)) {
-        return -EINVAL;
-    }
+	if (strlen(hash_str) != (IMAGE_HASH_SIZE * 2)) {
+		return -EINVAL;
+	}
 
-    ret = unhexlify(hash_str, hash, IMAGE_HASH_SIZE);
+	ret = unhexlify(hash_str, hash, IMAGE_HASH_SIZE);
 
-    if (ret == IMAGE_HASH_SIZE) {
-        ret = 0;
-    } else if (ret >= 0) {
-        ret = -EINVAL;
-    }
-    return ret;
+	if (ret == IMAGE_HASH_SIZE) {
+		ret = 0;
+	} else if (ret >= 0) {
+		ret = -EINVAL;
+	}
+	return ret;
 }
 
 
 #define MEMCPY_FLAG(_flag, _buf, _off, _len) \
-    do { \
-        const char flag[] = _flag ","; \
-        memcpy(_buf + _off, flag, sizeof(flag)); \
-        _off += (sizeof(flag) - 1); \
-        _len -= (sizeof(flag) - 1); \
-    } while (0)
+	do { \
+		const char flag[] = _flag ","; \
+		memcpy(_buf + _off, flag, sizeof(flag)); \
+		_off += (sizeof(flag) - 1); \
+		_len -= (sizeof(flag) - 1); \
+	} while (0)
 
 int slot_flags_to_str(char *flags_buf, const struct mgmt_slot_state *slot)
 {
-    size_t flags_len = SLOT_FLAGS_STR_MAX;
-    size_t flags_off = 0;
+	size_t flags_len = SLOT_FLAGS_STR_MAX;
+	size_t flags_off = 0;
 
-    if (slot->active) {
-        MEMCPY_FLAG("active", flags_buf, flags_off, flags_len);
-    }
+	if (slot->active) {
+		MEMCPY_FLAG("active", flags_buf, flags_off, flags_len);
+	}
 
-    if (slot->confirmed) {
-        MEMCPY_FLAG("confirmed", flags_buf, flags_off, flags_len);
-    }
+	if (slot->confirmed) {
+		MEMCPY_FLAG("confirmed", flags_buf, flags_off, flags_len);
+	}
 
-    if (slot->pending) {
-        MEMCPY_FLAG("pending", flags_buf, flags_off, flags_len);
-    }
+	if (slot->pending) {
+		MEMCPY_FLAG("pending", flags_buf, flags_off, flags_len);
+	}
 
-    if (slot->permanent) {
-        MEMCPY_FLAG("permanent", flags_buf, flags_off, flags_len);
-    }
+	if (slot->permanent) {
+		MEMCPY_FLAG("permanent", flags_buf, flags_off, flags_len);
+	}
 
-    if (flags_off) {
-        flags_buf[flags_off - 1] = '\0';
-    } else {
-        flags_buf[0] = '\0';
-    }
-    return 0;
+	if (flags_off) {
+		flags_buf[flags_off - 1] = '\0';
+	} else {
+		flags_buf[0] = '\0';
+	}
+	return 0;
 }
 
 #undef MEMCPY_FLAG
 
 void mcuboot_image_info_print(const struct mcuboot_image *image_info)
 {
-    char version_buf[IMAGE_VERSION_STR_MAX];
-    char hash_buf[IMAGE_HASH_STR_MAX];
+	char version_buf[IMAGE_VERSION_STR_MAX];
+	char hash_buf[IMAGE_HASH_STR_MAX];
 
-    image_version_to_str(version_buf, &image_info->version);
-    image_hash_to_str(hash_buf, image_info->hash);
+	image_version_to_str(version_buf, &image_info->version);
+	image_hash_to_str(hash_buf, image_info->hash);
 
-    printf("version:%s hash:%s size:%d filesize:%d\n", version_buf, hash_buf, image_info->img_sz, image_info->file_sz);
+	printf("version:%s hash:%s size:%d filesize:%d\n", version_buf, hash_buf, image_info->img_sz, image_info->file_sz);
 }
 
 void print_image_slot(struct mgmt_slot_state *slot)
 {
-    char version_buf[IMAGE_VERSION_STR_MAX];
-    char hash_buf[IMAGE_HASH_STR_MAX];
-    char flags[SLOT_FLAGS_STR_MAX];
+	char version_buf[IMAGE_VERSION_STR_MAX];
+	char hash_buf[IMAGE_HASH_STR_MAX];
+	char flags[SLOT_FLAGS_STR_MAX];
 
-    image_version_to_str(version_buf, &slot->version);
-    image_hash_to_str(hash_buf, slot->hash);
-    slot_flags_to_str(flags, slot);
+	image_version_to_str(version_buf, &slot->version);
+	image_hash_to_str(hash_buf, slot->hash);
+	slot_flags_to_str(flags, slot);
 
-    const char *bootable = slot->bootable ? "true" : "false";
+	const char *bootable = slot->bootable ? "true" : "false";
 
-    printf("slot:%d version:%s hash:%s bootable:%s flags:%s\n", slot->slot, version_buf, hash_buf, bootable, flags);
+	printf("slot:%d version:%s hash:%s bootable:%s flags:%s\n", slot->slot, version_buf, hash_buf, bootable, flags);
 }
 
 
 void print_image_slot_state(struct mgmt_image_state *slot_state)
 {
-    for (int slot_n = 0; slot_n < slot_state->num_slots; ++slot_n) {
-        print_image_slot(&slot_state->slot[slot_n]);
-    }
-    if (slot_state->split_status) {
-        printf("SplitStatus: %d\n", (int)slot_state->split_status);
-    } else {
-        printf("SplitStatus: N/A\n");
-    }
+	for (int slot_n = 0; slot_n < slot_state->num_slots; ++slot_n) {
+		print_image_slot(&slot_state->slot[slot_n]);
+	}
+	if (slot_state->split_status) {
+		printf("SplitStatus: %d\n", (int)slot_state->split_status);
+	} else {
+		printf("SplitStatus: N/A\n");
+	}
 }
